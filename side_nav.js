f55fc85a0bead84d50630615df2765bb6068d86a
Akinwande Tomisin <tomjames156@gmail.com>
2022-06-03 05:02:19 -0500

    sidenav overlay created

--- side_nav.js
+++ side_nav.js
@@ -4,22 +4,9 @@ const sidebar = document.querySelector(".side-nav");
 const mainContent = document.querySelector(".main");
 
 function showSideBar(){
-    sidebar.style.width = "17%";
-    mainContent.style.marginLeft = "19.5%";
-    // sidebar.style.display = "block";
-    // sidebar.classList.add("show");
-    // sidebar.classList.remove("hide");
+    sidebar.style.width = "17vw";
 }
 
 function closeSideBar(){
-    sidebar.style.width = "0";
-    // sidebar.style.display = "none";
-    mainContent.style.marginLeft = "0"
-    // sidebar.classList.add("hide");
-    // sidebar.classList.remove("show");
-    // setTimeout(remove, 499);
-}
-
-function remove(){
-    sidebar.style.display = "none";
+    sidebar.style.width = 0;   
 }

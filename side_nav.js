1068f80126aa6e83bc87c92cadcd331b31cbe202
Akinwande Tomisin <tomjames156@gmail.com>
2022-06-04 03:01:02 -0500

    added opacity to animation

--- side_nav.js
+++ side_nav.js
@@ -4,11 +4,13 @@ const sidebar = document.querySelector(".side-nav");
 const mainContent = document.querySelector(".main");
 
 function showSideBar(){
+    mainContent.style.opacity = "40%";
     sidebar.style.width = "17vw";
     mainContent.style.marginLeft = "17vw";
 }
 
 function closeSideBar(){
     sidebar.style.width = "0";
-    mainContent.style.marginLeft = "0";
+    mainContent.style.marginLeft = "0";    
+    mainContent.style.opacity = "unset";
 }

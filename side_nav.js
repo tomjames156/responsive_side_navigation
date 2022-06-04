4ca3430576b445fe499962fb2a4a0a1329091b5d
Akinwande Tomisin <tomjames156@gmail.com>
2022-06-04 03:08:12 -0500

    updated animation

--- side_nav.js
+++ side_nav.js
@@ -4,9 +4,9 @@ const sidebar = document.querySelector(".side-nav");
 const mainContent = document.querySelector(".main");
 
 function showSideBar(){
-    mainContent.style.opacity = "40%";
     sidebar.style.width = "17vw";
     mainContent.style.marginLeft = "17vw";
+    mainContent.style.opacity = "40%";
 }
 
 function closeSideBar(){

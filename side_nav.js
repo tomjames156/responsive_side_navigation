0c9d270f6493e1f1c2eb0359c30fad4b400f0aea
Akinwande Tomisin <tomjames156@gmail.com>
2022-06-06 02:48:11 -0500

    added opaque  black background

--- side_nav.js
+++ side_nav.js
@@ -6,11 +6,11 @@ const mainContent = document.querySelector(".main");
 function showSideBar(){
     sidebar.style.width = "17vw";
     mainContent.style.marginLeft = "17vw";
-    mainContent.style.opacity = "40%";
+    mainContent.style.background = "rgba(0, 0, 0, 0.4)";
 }
 
 function closeSideBar(){
     sidebar.style.width = "0";
     mainContent.style.marginLeft = "0";    
-    mainContent.style.opacity = "unset";
+    mainContent.style.background = "white";
 }
